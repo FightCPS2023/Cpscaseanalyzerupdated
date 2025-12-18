fe97775f0ef342e7ecedb6e07d3434d8e90c57b9
fightcps2023 <darrenguay@gmail.com>
2025-12-18 04:49:34 -0500

    Clean up duplicate configs and fix Tailwind/PostCSS root setup

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {
+      overrideBrowserslist: [
+        '>0.2%',
+        'not dead',
+        'not op_mini all',
+      ],
+    },
+  },
+}
+
+
